98a680a38ecf749270464f9a3e54aadff9de6620
jagaldol <yoy06056@pusan.ac.kr>
2023-07-10 21:15:05 +0900

    feat: 명암비 및 resize 추가

--- nuki/rem2can.py
+++ nuki/rem2can.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     OUTPUT_DIR =  os.path.join(FILE_DIR, 'outputs')
     CROP_DIR = os.path.join(FILE_DIR, 'crops')
 
-    INPUT_NAME = 'box1.png'
+    INPUT_NAME = 'box9.png'
     OUTPUT_NAME = 'box.png'
     CROP_NAME = 'crop.png'
 
@@ -26,8 +26,14 @@ if __name__ == '__main__':
             os.makedirs(CROP_PATH)
     # PATH 세팅 끝
 
-    #이미지를 GRAY SCALE로 읽기
+    # 이미지를 GRAY SCALE로 읽기
     input = cv2.imread(INPUT_PATH, 0)
+    # TODO: 현재 crop 이미지가 정사각형으로 오는게 아니기 때문에 원본 비율을 기록해두거나 비율을 보존한 resize가 필요함
+    input = cv2.resize(input, (256,256))
+
+    # 명암비 alpha 0이면 그대로, 양수일수록 명암비가 커진다.
+    alpha = 0.5
+    input = np.clip((1+alpha) * input - 128 * alpha, 0, 255).astype(np.uint8)
     
     # 배경 제거, 이때 배경은 검정
     output = remove(input,
@@ -46,7 +52,7 @@ if __name__ == '__main__':
     cv2.imshow('nuki', nuki)
 
     # morphology를 위한 kernel 제작 nxn의 kernel로 사각형(MORPH_RECT), 즉 커널이 전부 1로 채워짐
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
     # 커널을 사용해 MORPH_CLOSE -> 커널에 맞게 주변 픽셀 다 선택해서 채우기 때문에 선이 두꺼워진다.
     morhpology = cv2.morphologyEx(nuki, cv2.MORPH_CLOSE, kernel)
 
